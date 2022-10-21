54897f25c22f120a145747b6997499fafc006e49
yummyweb <antriksh.timepass@gmail.com>
2022-10-21 17:19:07 +0530

    added example code

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cudos_todo_athena::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
